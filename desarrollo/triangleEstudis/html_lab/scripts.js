b332a6871b5df96f705c8c968650a03f27b7e2f1
drejon <84577709+drejon@users.noreply.github.com>
2023-03-13 17:43:58 +0100

    Delete desarrollo/triangleEstudis/html_lab directory

--- desarrollo/triangleEstudis/html_lab/scripts.js
+++
@@ -1,7 +0,0 @@
-function generateDivs(num){
-    let main = document.getElementById('main');
-    for (let i = 0; i < num; i++){
-        let div = document.createElement('div');
-        main.appendChild(div);
-    }
-}
